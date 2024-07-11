8c00188107184b1d45b6cee3a549dd7eb9f97e96
Mahmoud Ahmed <Mahmoud Ahmed@DESKTOP-3CFC79A>
2024-07-11 13:00:55 +0300

    Add project files.

---
+++ Demo/Car.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Car
+    {
+
+        #region Atrributes
+
+        private int id;
+
+        private string model;
+
+        private double speed;
+
+        #endregion
+
+        #region Properties
+
+        public double Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        //if the isn't a user-defined constructor, Compiler will Generate the Parameterless constructor
+
+        ///public Car()
+        ///{
+        ///}
+
+        //if the isn a user-defined constructor, Compiler will not Generate the Parameterless constructor
+
+        public Car(int id,string model,double speed)
+        {
+            this.id = id;
+            this.model = model;
+            this.speed = speed;
+            Console.WriteLine("Constructor 1");
+        }
+
+        public Car(int id, string model) : this(id, model, 200) // Constructor Chaining
+        {
+            Console.WriteLine("Constructor 2");
+        }
+
+        public Car(int id):this(id, "Mercedes Benz",250)
+        {
+            Console.WriteLine("Constructor 3");
+        }
+
+
+        #endregion
+
+        #region Methods
+
+        public override string ToString()
+        {
+            return $"Car Id: {id}\nModel: {model}\nSpeed: {speed} K/m";
+        }
+
+
+        #endregion
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car c1;
+
+            /// Declare for Reference of type "Car", Refering to NULL
+            /// CLR will Allocate 4 byte at STACK for this reference "C1"
+            /// ClR will Allocate 0 byte at HEAP  
+            /// 
+
+            //c1 = new Car(123, "Mercedes G-Class", 200);
+
+            /// new
+            /// 1. Allocate Required number of bytes for the allocated object at HEAP = 16 Bytes
+            /// 2. Initialize the allocated bytes of each and every attribute with the default value of attribute datatype
+            /// 3. Call User-defined Constructor [if exists]    
+            /// 4. Assign the address of the allocated object in Heap to the references "c1"
+            ///
+            
+            c1 = new Car(123, "Mercedes G-Class", 200);
+
+            Console.WriteLine(c1);
+
+            Console.WriteLine("\n\n-------------------------------------\n\n");
+
+            //----------------------------------------------//
+
+            c1 = new Car(123, "Hundai");
+
+            Console.WriteLine(c1);
+
+            Console.WriteLine("\n\n-------------------------------------\n\n");
+
+            //---------------------------------------------//
+
+            c1 = new Car(312);
+
+            Console.WriteLine(c1);
+
+
+        }
+    }
+}
